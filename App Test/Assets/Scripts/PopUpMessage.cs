8e8f066aeea7f987adb1f7ee8ab9fa0fb955a85d
lanphuong186 <lanphuong18694@gmail.com>
2022-06-01 16:14:39 +0200

    Lan
    LanBox

--- App Test/Assets/Scripts/PopUpMessage.cs
+++ App Test/Assets/Scripts/PopUpMessage.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class PopUpMessage : MonoBehaviour
 {
 
     [SerializeField] Image MessageBox;
+    [SerializeField] Image HeaderBox;
     [SerializeField] TextMeshProUGUI messagerText;
+    [SerializeField] TextMeshProUGUI headerText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +23,6 @@ public class PopUpMessage : MonoBehaviour
             return;
         }
         DontDestroyOnLoad(this.gameObject);
-        //MessageBox = GetComponentInChildren<Image>();
-        //messagerText = MessageBox.GetComponentInChildren<TextMeshProUGUI>();
         SubscribeToEvents_Start();
     }
 
@@ -32,41 +33,78 @@ public class PopUpMessage : MonoBehaviour
     }
 
     public void ShowBoxNewTask(string t, string d, int[] dt, float prio, int repeatindex)
-    {
-        if (MessageBox != null)
+    //{
+        //if (MessageBox != null)
         {
-            Debug.Log("11111");
+            Debug.Log("111");
             StartCoroutine(ShowText("Task was created"));
         }
-    }
+    //}
 
     public void ShowBoxTaskChange(Taskmaster.Task oldtask, string t, string d, int[] dt, float p, int repeatIndex)
     {
-        Debug.Log("22222");
+        Debug.Log("222");
         StartCoroutine(ShowText("Task was changed"));
+    }
+
+    public void ShowBoxTaskDone(Taskmaster.Task doneTask)
+    {
+        Debug.Log("333");
+
+        DateTime expDate = ConvertIntArray_toDatetime(doneTask.Deadline);
+        DateTime localDate = System.DateTime.Now;
+        print(localDate);
+        int remain = (expDate - localDate).Days;
+        print(remain);
+
+        StartCoroutine(ShowText("You have completed the task" + " " + remain + " " + "days before deadline"));
+    }
+    public System.DateTime ConvertIntArray_toDatetime(int[] toconvert)
+    {
+        return new DateTime(toconvert[4], toconvert[3], toconvert[2], toconvert[1], toconvert[0], 0);
+    }
+
+    public void ShowBoxTaskReturn(Taskmaster.Task oldtask, string potNewname, string potNewDiscp, int[] potNewDt, float potNewPrioint, int repeatIndex)
+    {
+        Debug.Log("444");
+        StartCoroutine(ShowText("You have a new task to do"));
+    }
+
+    public void ShowBoxTaskExpired()
+    {
+        Debug.Log("555");
+        StartCoroutine(ShowText("A task has expired"));
     }
 
     IEnumerator ShowText(string text)
     {
-        MessageBox.color = new Color32(255, 255, 255, 100);
-        Debug.Log(33333);
-        //MessageBox.enabled = true;
+        HeaderBox.color = new Color32(0, 0, 0, 190);
+        MessageBox.color = new Color32(54, 99, 99, 212);
+        headerText.text = "Message!";
         messagerText.text = text;
-        yield return new WaitForSeconds(10);
+        yield return new WaitForSeconds(5);
         messagerText.text = "";
-        MessageBox.color = new Color32(0, 0, 0, 0);   
+        headerText.text = "";
+        MessageBox.color = new Color32(0, 0, 0, 0);
+        HeaderBox.color = new Color32(0, 0, 0, 0);
     }
 
     public void SubscribeToEvents_Start()
     {
         Subject.current.OnNewTask += ShowBoxNewTask;
         Subject.current.OnTaskChange += ShowBoxTaskChange;
+        Subject.current.OnTaskSetDone += ShowBoxTaskDone;
+        Subject.current.OnTaskReturning += ShowBoxTaskReturn;
+        Subject.current.OnExpiredDealine += ShowBoxTaskExpired;
     }
 
     public void UnsubscribeToAllEvent()
     {
         Subject.current.OnNewTask -= ShowBoxNewTask;
         Subject.current.OnTaskChange -= ShowBoxTaskChange;
+        Subject.current.OnTaskSetDone -= ShowBoxTaskDone;
+        Subject.current.OnTaskReturning -= ShowBoxTaskReturn;
+        Subject.current.OnExpiredDealine -= ShowBoxTaskExpired;
     }
 
     private void OnEnable()
