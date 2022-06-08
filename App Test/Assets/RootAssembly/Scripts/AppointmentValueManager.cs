f09baf6fbbca76a86e8a8e4249873e874d5bb79b
felix malsch <malsch.felix@web.de>
2022-06-08 21:28:50 +0200

    corrected typo

--- App Test/Assets/RootAssembly/Scripts/AppointmentValueManager.cs
+++ App Test/Assets/RootAssembly/Scripts/AppointmentValueManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AppointmenValueManager : ValueManager
+public class AppointmentValueManager : ValueManager
 {
 
     [SerializeField] private DatePicker endTimePicker;
